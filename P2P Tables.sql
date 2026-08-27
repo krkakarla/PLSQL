f19e8a0ca88e22d4c6eaca1513e6707357be3cab
krkakarla <95361251+krkakarla@users.noreply.github.com>
2026-08-27 20:16:19 +0530

    P2P Format Change

--- P2P Tables.sql
+++ P2P Tables.sql
@@ -1,9 +1,10 @@
-////Invoice Details Table. 
+	I.Invoice Details Table. 
 We can see invoices details at below table however the transaction accounting details available at XLA event Level. 
 1.	select * from ap_invoices_all;
 2.	select * from ap_invoice_lines_all;
 3.	select * from ap_invoice_distributions_all where  Invoice_id = '108335958'; => Here we get accounting_event_id information.
-II.	Sub Ledger Journals Details.
+
+	II.	Sub Ledger Journals Details.
 4.	select * from XLA_EVENTS WHERE EVENT_ID = '5339125904';
 5.	select * from xla_ae_headers where event_id = '5339125904';
 6.	select * from xla_ae_lines where ae_header_id = '5330865516';
@@ -13,6 +14,7 @@ SELECT * FROM GL_JE_BATCHES WHERE NAME = 'Payable  % ';
 SELECT * FROM GL_JE_HEADERS WHERE JE_BATCH_ID = '2927184';
 SELECT * FROM GL_JE_LINES WHERE HE_HEADER_ID = '' Status U It means data not posted. 
 Technical Table Payment Details 
+	
 Before we make payment then we will get the invoices details at below table.
 This is Journal entry after making the payment. 
 S.No	Particulars	Debit 	Credit
@@ -29,6 +31,7 @@ Payment
 6.	select * from iby_pay_service_requests;
 7.	select * from iby_payments_all where payment_id = '13955877';
 8.	select * from iby_pay_service_requests where payment_service_request_id = '279390'
+	
 Payment Sub ledger Accounting 
 7.	select accounting_event_id from ap_invoice_payments_all where INVOICE_ID = '108335958';
 8.	select * from xla_events where EVENT_ID = '5339125905';
